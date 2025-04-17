404a867fb48c89a34adce2692cd94bbb941b794f
Viktor Shterev <viktor.shterev55@gmail.com>
2025-04-17 09:21:01 +0300

    adding customer dashboard view

---
+++ src/main/java/my/projects/lockersystemusermicroservice/web/CustomerDashboardController.java
@@ -0,0 +1,14 @@
+package my.projects.lockersystemusermicroservice.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CustomerDashboardController {
+
+    @GetMapping("/customer-dashboard")
+    public ModelAndView customerDashboard() {
+        return new ModelAndView("customer-dashboard");
+    }
+}
