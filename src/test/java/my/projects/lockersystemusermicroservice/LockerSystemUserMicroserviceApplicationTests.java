56640d03aebf7eba27f1948d7af3a82667cc3478
Viktor Shterev <viktor.shterev55@gmail.com>
2025-05-02 11:10:52 +0300

    adding pom and application.yaml

---
+++ src/test/java/my/projects/lockersystemusermicroservice/LockerSystemUserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.projects.lockersystemusermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerSystemUserMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
