4855f65a26f8d8bbc15a1155775bcc569356504f
Viktor Shterev <viktor.shterev55@gmail.com>
2025-05-08 10:29:35 +0300

    authentication fixes

--- src/main/java/my/projects/lockersystemusermicroservice/service/AuthenticationService.java
+++ src/main/java/my/projects/lockersystemusermicroservice/service/AuthenticationService.java
@@ -1,9 +1,13 @@
 package my.projects.lockersystemusermicroservice.service;
 
 import my.projects.lockersystemusermicroservice.dto.RegisterUserDTO;
+import my.projects.lockersystemusermicroservice.entity.UserDetails;
+
+import java.util.Optional;
 
 public interface AuthenticationService {
 
     boolean register(RegisterUserDTO registerUserDTO);
 
+    Optional<UserDetails> getCurrentUser();
 }
--- src/main/java/my/projects/lockersystemusermicroservice/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/my/projects/lockersystemusermicroservice/service/impl/AuthenticationServiceImpl.java
@@ -2,11 +2,17 @@ package my.projects.lockersystemusermicroservice.service.impl;
 
 import my.projects.lockersystemusermicroservice.dto.RegisterUserDTO;
 import my.projects.lockersystemusermicroservice.entity.User;
+import my.projects.lockersystemusermicroservice.entity.UserDetails;
 import my.projects.lockersystemusermicroservice.enums.Role;
 import my.projects.lockersystemusermicroservice.repository.UserRepository;
 import my.projects.lockersystemusermicroservice.service.AuthenticationService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -29,9 +35,20 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User(registerUserDTO.getFullName(),
                 registerUserDTO.getEmail(),
                 this.passwordEncoder.encode(registerUserDTO.getPassword()),
-                Role.valueOf(registerUserDTO.getRole()));
+                Role.valueOf(registerUserDTO.getRole()),
+                UUID.randomUUID());
 
         this.userRepository.save(user);
         return true;
+    }
+
+    @Override
+    public Optional<UserDetails> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null &&
+                authentication.getPrincipal() instanceof UserDetails userDetails) {
+            return Optional.of(userDetails);
+        }
+        return Optional.empty();
     }
 }
