eeedf73673ecf6d293b1062f85a2a0413cca34b3
Viktor Shterev <viktor.shterev55@gmail.com>
2025-05-13 09:34:36 +0300

    fixing courier controller

--- src/main/java/my/projects/lockersystemusermicroservice/web/CourierDashboardController.java
+++ src/main/java/my/projects/lockersystemusermicroservice/web/CourierDashboardController.java
@@ -2,12 +2,14 @@ package my.projects.lockersystemusermicroservice.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/courier")
 public class CourierDashboardController {
 
-    @GetMapping("/courier-dashboard")
+    @GetMapping("/dashboard")
     public ModelAndView courierDashboard() {
         return new ModelAndView("courier-dashboard");
     }
